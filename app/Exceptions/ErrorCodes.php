37652a7730fc2c6495931e7d2e2cff63af6367e6
forecho <caizhenghai@gmail.com>
2021-12-13 13:56:09 +0800

    feat: add exceptionHandler

--- app/Exceptions/ErrorCodes.php
+++ app/Exceptions/ErrorCodes.php
@@ -4,7 +4,10 @@ namespace App\Exceptions;
 
 class ErrorCodes
 {
+    public const INVALID_ARGUMENT_ERROR = 42000;
+    public const RECORD_NOT_FOUND = 42001;
     public const UNAUTHENTICATED = 42002;
+    public const TOO_MANY_REQUESTS = 42008;
 
 
     public const INTERNAL_ERROR = 50000;
--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,16 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -10,7 +19,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<Throwable>>
+     * @var string[]
      */
     protected $dontReport = [
         //
@@ -19,7 +28,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
-     * @var array<int, string>
+     * @var string[]
      */
     protected $dontFlash = [
         'current_password',
@@ -37,5 +46,100 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function report(Throwable $e)
+    {
+        if (app()->bound('sentry') && $this->shouldReport($e)) {
+            app('sentry')->captureException($e);
+        }
+
+        parent::report($e);
+    }
+
+
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Throwable  $e
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response|Response
+     */
+    public function render($request, Throwable $e)
+    {
+        $message = $e->getMessage();
+        $exceptionCode = $e->getCode() > 0 ? $e->getCode() : ErrorCodes::INTERNAL_ERROR;
+        $statusCode = $this->getStatusCode($e);
+        $eClass = get_class($e);
+
+        switch ($eClass) {
+            case ValidationException::class:
+                $exceptionCode = ErrorCodes::INVALID_ARGUMENT_ERROR;
+                /** @var ValidationException $e */
+                $message = $e->validator->errors()->first();
+
+                break;
+            case ModelNotFoundException::class:
+                $exceptionCode = ErrorCodes::RECORD_NOT_FOUND;
+                $message = 'No query results';
+
+                break;
+            case QueryException::class:
+                // SQL error
+                $exceptionCode = ErrorCodes::INTERNAL_ERROR;
+                if (!config('app.debug')) {
+                    $exceptionCode = ErrorCodes::INTERNAL_ERROR;
+                    $message = 'SQL error';
+                }
+
+                break;
+            case AuthenticationException::class:
+                $exceptionCode = ErrorCodes::UNAUTHENTICATED;
+                $statusCode = Response::HTTP_UNAUTHORIZED;
+
+                break;
+            default:
+                switch ($statusCode) {
+                    case Response::HTTP_NOT_FOUND:
+                        $message = 'Not found';
+
+                        break;
+                    case Response::HTTP_METHOD_NOT_ALLOWED:
+                        $message = 'Method Not Allowed';
+
+                        break;
+                    case Response::HTTP_TOO_MANY_REQUESTS:
+                        $exceptionCode = ErrorCodes::TOO_MANY_REQUESTS;
+
+                        break;
+                    default:
+                }
+
+                break;
+        }
+
+        if (!($e instanceof NotFoundHttpException || $e instanceof MethodNotAllowedHttpException)) {
+            Log::error(
+                'ExceptionHandler',
+                [
+                    'url' => $request->fullUrl(),
+                    'message' => $message,
+                    'code' => $exceptionCode,
+                    'request' => (array)$request->getContent(),
+                    'exception' => (string)$e,
+                ],
+            );
+        }
+        $data = ['code' => $exceptionCode, 'message' => $message];
+        if (config('app.debug')) {
+            $data['trace'] = $e->getTrace();
+        }
+
+        return response()->json($data, $statusCode);
+    }
+
+
+    protected function getStatusCode(Throwable $e): int
+    {
+        return $e instanceof HttpExceptionInterface ?
+            $e->getStatusCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
     }
 }
